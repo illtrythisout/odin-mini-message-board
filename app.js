d0d3574121a109da62f0d59612a0b9b137d44da9
Theo <170449321+illtrythisout@users.noreply.github.com>
2025-05-10 14:54:59 +0200

    chore: set up boilerplate code in app.js

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const app = express(); // initialize express in app
+
+const path = require('node:path');
+
+// Allow's the app to correctly parse the incoming request’s body
+app.use(express.urlencoded({ extended: true }));
+
+// Set up EJS as the view engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// Allow to use static assets from public folder
+const assetsPath = path.join(__dirname, 'public');
+app.use(express.static(assetsPath));
+
+// Temporary root get
+app.get('/', (req, res) => res.send('Hello, World!'));
+
+// To handle errors
+// Every thrown error in the application or the previous middleware function calling `next` with an error as an argument will eventually go to this middleware function
+app.use((err, req, res, next) => {
+  console.error(err);
+  // We specify the `err.statusCode` that exists in our custom error class and if it does not exist it's probably an internal server error
+  res.status(err.statusCode || 500).send(err.message);
+});
+
+// Start server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Express app - listening on port ${PORT}!`);
+});
