2aff22f4e082bc28e45c33f2b01738754a7ad161
Theo <170449321+illtrythisout@users.noreply.github.com>
2025-05-11 18:35:16 +0200

    feat: style pages

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -5,8 +5,8 @@ const messages = [
   { text: 'Oooh Weee', user: 'Mr. Poopybutthole', added: new Date() },
   { text: 'Get Schwifty', user: 'Big Head', added: new Date() },
   {
-    text: 'Get out of my personal space!',
-    user: 'Philip Jacobs Show',
+    text: "Hi, I'm Mr. Meeseeks!",
+    user: 'Mr. Meeseeks',
     added: new Date(),
   },
 ];
